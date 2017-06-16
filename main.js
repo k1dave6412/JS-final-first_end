6778a91476a90a630193cc48d16f1214e8b9981b
k1dave6412 <k1dave6412@gmail.com>
2017-06-17 02:01:30 +0800

    lesson16

--- main.js
+++ main.js
@@ -27,13 +27,48 @@ towerImg.src = "images/tower.png";
 var enemy = {
     x: 32 * 3,
     y: 480 - 32,
+
+    pathDes: 0,
     speedX: 0,
     speedY: -64,
     move: function move() {
-        this.x = this.x + this.speedX / FPS;
-        this.y = this.y + this.speedY / FPS;
+        if (isCollided(this.x, this.y, enemyPath[this.pathDes].x, enemyPath[this.pathDes].y, 64 / FPS, 64 / FPS)) {
+            this.pathDes += 1;
+            if (this.pathDes !== enemyPath.length) {
+
+                if (enemyPath[this.pathDes].x > enemyPath[this.pathDes - 1].x) {
+                    this.speedX = 64;
+                    this.speedY = 0;
+                } else if (enemyPath[this.pathDes].x < enemyPath[this.pathDes - 1].x) {
+                    this.speedX = -64;
+                    this.speedY = 0;
+                } else if (enemyPath[this.pathDes].y > enemyPath[this.pathDes - 1].y) {
+                    this.speedX = 0;
+                    this.speedY = 64;
+                } else {
+                    this.speedX = 0;
+                    this.speedY = -64;
+                }
+            } else {
+                this.speedX = 0;
+                this.speedY = 0;
+            }
+        }
+        this.x += this.speedX / FPS;
+        this.y += this.speedY / FPS;
     },
 };
+
+// enemy nav arrays
+var enemyPath = [
+    { x: 32 * 3, y: 32 * 2 },
+    { x: 32 * 12, y: 32 * 2 },
+    { x: 32 * 12, y: 32 * 6 },
+    { x: 32 * 7, y: 32 * 6 },
+    { x: 32 * 7, y: 32 * 10 },
+    { x: 32 * 17, y: 32 * 10 },
+    { x: 32 * 17, y: 32 * 3 },
+]
 
 // cursor get
 var cursor = {
@@ -59,7 +94,13 @@ function draw() {
     ctx.drawImage(characterImg, 640 - 32, 480 - 32 * 3);
     ctx.drawImage(enemyImg, enemy.x, enemy.y);
     ctx.drawImage(buildImg, 640 - 32 * 2, 480 - 32 * 2, 32 * 2, 32 * 2);
-    ctx.drawImage(towerImg, tower.x, tower.y)
+
+    if (isBuilding) {
+        ctx.drawImage(towerImg, cursor.x, cursor.y)
+    } else {
+        ctx.drawImage(towerImg, tower.x, tower.y)
+    }
+
     enemy.move();
 }
 
@@ -71,12 +112,9 @@ setInterval(draw, 1000 / FPS);
 //get cursor on canvas
 $("#canvas").on("mousemove",
     function(event) {
-        cursor.x = event.offsetX;
-        cursor.y = event.offsetY;
-
         if (isBuilding) {
-            tower.x = event.offsetX - event.offsetX % 32;
-            tower.y = event.offsetY - event.offsetY % 32;
+            cursor.x = event.offsetX - event.offsetX % 32;
+            cursor.y = event.offsetY - event.offsetY % 32;
         }
     }
 );
@@ -84,19 +122,29 @@ $("#canvas").on("mousemove",
 //judge click on buildingImg
 $("#canvas").on("click",
     function(event) {
-        if (cursor.x > 640 - 32 * 2 && cursor.y > 480 - 32 * 2) {
-            if (isBuilding) {
-                isBuilding = false;
-            } else {
-                isBuilding = true;
-            }
-        } else {
-            if (isBuilding) {
-                isBuilding = false;
-            }
+        if (event.offsetX > 640 - 32 * 2 && event.offsetY > 480 - 32 * 2) {
+            isBuilding = !isBuilding;
+        } else if (isBuilding) {
+            tower.x = cursor.x;
+            tower.y = cursor.y;
+            isBuilding = false;
         }
     }
 );
+
+// collieded?
+function isCollided(pointX, pointY, targetX, targetY, targetWidth, targetHeight) {
+    if (pointX >= targetX &&
+        pointX <= targetX + targetWidth &&
+        pointY >= targetY &&
+        pointY <= targetY + targetHeight
+    ) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
 
 
 
