21287cc79bdac50d393a7898f3c597a3e5c663ba
k1dave6412 <k1dave6412@gmail.com>
2017-06-17 01:22:21 +0800

    lesson13

--- main.js
+++ main.js
@@ -29,80 +29,15 @@ var enemy = {
     y: 480 - 32,
 };
 
-//cursor get
-var cursor = {
-    x: 0,
-    y: 0,
-}
-
-//tower cursor
-var tower = {
-    x: 0,
-    y: 0,
-}
-
-//isBuilding
-var isBuilding = false;
-
-
 function draw() {
     ctx.drawImage(bgImg, 0, 0);
     ctx.drawImage(characterImg, 640 - 32, 480 - 32 * 3);
     ctx.drawImage(enemyImg, enemy.x, enemy.y);
     ctx.drawImage(buildImg, 640 - 32 * 2, 480 - 32 * 2, 32 * 2, 32 * 2);
-    ctx.drawImage(towerImg, tower.x, tower.y)
+
 }
 
 // delay loading
 setTimeout(draw, 100);
 // repeat loading
 setInterval(draw, 30)
-
-//get cursor on canvas
-$("#canvas").on("mousemove",
-    function(event) {
-        cursor.x = event.offsetX;
-        cursor.y = event.offsetY;
-
-        if (isBuilding) {
-            tower.x = event.offsetX - event.offsetX % 32;
-            tower.y = event.offsetY - event.offsetY % 32;
-        }
-    }
-);
-
-//judge click on buildingImg
-$("#canvas").on("click",
-    function(event) {
-        if (cursor.x > 640 - 32 * 2 && cursor.y > 480 - 32 * 2) {
-            if (isBuilding) {
-                isBuilding = false;
-            } else {
-                isBuilding = true;
-            }
-        } else {
-            if (isBuilding) {
-                isBuilding = false;
-            }
-        }
-    }
-);
-
-
-
-/* 
-canvas drawImg:
-ctx.drawImage(img, x, y, width, height) 
-
-mouse offset in target:
-$("#target").on("mousemove",
-    function(event) {
-        console.log("x: " + event.offsetX + ",y: " + event.offsetY)
-    }
-);
-$("#target").on("click",
-    function(event) {
-        console.log("x: " + event.offsetX + ",y: " + event.offsetY)
-    }
-);
-*/
